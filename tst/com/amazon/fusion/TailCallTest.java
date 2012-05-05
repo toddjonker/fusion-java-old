fe85705ca1f9455d1a1831c26d69a2c82f06f90e
Todd V. Jonker <todd@toddjonker.net>
2012-05-04 17:36:21 -0700

    Tail-call tests run faster.

--- tst/com/amazon/fusion/TailCallTest.java
+++ tst/com/amazon/fusion/TailCallTest.java
@@ -7,6 +7,30 @@ import org.junit.Test;
 public class TailCallTest
     extends CoreTestCase
 {
+    private static final int STACK_OVERFLOW_DEPTH = 8000;
+
+    /**
+     * Test that {@link #STACK_OVERFLOW_DEPTH} is large enough to cause a
+     * stack overflow, otherwise the other tests below are not actually
+     * testing anything.  We want to keep that number low so tests run fast.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void testStackOverflowDepth()
+        throws Exception
+    {
+        eval("(define countup" +
+            "  (lambda (i limit)" +
+            "    (if (= i limit) i" +
+            "      (+ 1 (countup i (- limit 1))))))");
+
+        // Make sure the procedure is working right.
+        assertEval(10, "(countup 0 10)");
+
+        // Force a stack overflow
+        eval("(countup 0 " + STACK_OVERFLOW_DEPTH + ")");
+    }
+
+
     @Test
     public void testTailCall()
         throws Exception
@@ -19,6 +43,8 @@ public class TailCallTest
              "        (let ((x 1))" +
                "        (letrec ((v 5))" +
              "            (countup (+ 1 i) limit)))))))");
-        assertEval(1000000, "(countup 0 1000000)");
+
+        assertEval(STACK_OVERFLOW_DEPTH,
+                   "(countup 0 " + STACK_OVERFLOW_DEPTH + ")");
     }
 }
