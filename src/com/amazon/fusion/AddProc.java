4bee4c8f3b4e75ecb3aa3c2ff0292c38ce3c9fe6
Jason Lee <leejason@amazon.com>
2012-07-20 17:18:05 -0700

    Fixed AddProc whitespace error

---
+++ src/com/amazon/fusion/AddProc.java
@@ -0,0 +1,36 @@
+// Copyright (c) 2012 Amazon.com, Inc. All rights reserved.
+
+package com.amazon.fusion;
+
+import static com.amazon.fusion.FusionUtils.cloneIfContained;
+import com.amazon.ion.IonSequence;
+import com.amazon.ion.IonValue;
+
+class AddProc
+    extends Procedure
+{
+    AddProc()
+    {
+        super("Adds the Ion DATUM to the end of the SEQUENCE (IonList or sexp).",
+              "sequence","datum");
+    }
+
+    static void invoke(IonSequence seq, IonValue value)
+    {
+        value = cloneIfContained(value);
+        seq.add(value);
+    }
+
+    @Override
+    FusionValue invoke(Evaluator eval, FusionValue[] args)
+        throws FusionException
+    {
+         checkArityExact(2,args);
+         IonSequence seq = checkSequenceArg(0,args);
+         IonValue value = checkIonArg(1,args);
+
+         invoke(seq,value);
+
+         return args[0];
+    }
+}
