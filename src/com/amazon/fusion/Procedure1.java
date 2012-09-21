e2cf98e5115f2bc38e80d6c94d835d407159d4c9
Todd V. Jonker <todd@toddjonker.net>
2012-09-21 12:05:50 -0700

    Added Procedure1

---
+++ src/com/amazon/fusion/Procedure1.java
@@ -0,0 +1,28 @@
+// Copyright (c) 2012 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion;
+
+/**
+ * A procedure of exactly one argument.
+ * This class performs arity checking before invoking the subclass
+ * implementation of {@link #doApply(Evaluator, Object)}.
+ */
+abstract class Procedure1
+    extends Procedure
+{
+    Procedure1(String doc, String argName)
+    {
+        super(doc, argName);
+    }
+
+    abstract Object doApply(Evaluator eval, Object arg)
+        throws FusionException;
+
+    @Override
+    final Object doApply(Evaluator eval, Object[] args)
+        throws FusionException
+    {
+        checkArityExact(1, args);
+        return doApply(eval, args[0]);
+    }
+}
