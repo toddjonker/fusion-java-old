04d625b2bf2bf595637f7c767eeae537ec5642ea
Todd V. Jonker <todd@toddjonker.net>
2012-11-06 14:21:08 -0800

    Moved class Closure in with LambdaForm.

--- src/com/amazon/fusion/LambdaForm.java
+++ src/com/amazon/fusion/LambdaForm.java
@@ -163,4 +163,42 @@ final class LambdaForm
             return new Closure(store, myDoc, myArgNames, myBody);
         }
     }
+
+
+    private static final class Closure
+        extends Procedure
+    {
+        private final Store        myEnclosure;
+        private final CompiledForm myBody;
+
+
+        /**
+         * Constructs a new closure from its source and enclosing lexical
+         * environment.
+         *
+         * @param enclosure the store lexically surrounding the source of this
+         *  closure.  Any free variables in the procedure are expected to be
+         *  bound here.
+         */
+        Closure(Store enclosure, String doc, String[] argNames,
+                CompiledForm body)
+        {
+            super(doc, argNames);
+
+            myEnclosure = enclosure;
+            myBody      = body;
+        }
+
+
+        @Override
+        Object doApply(Evaluator eval, final Object[] args)
+            throws FusionException
+        {
+            checkArityExact(args);
+
+            Store localStore = new LocalStore(myEnclosure, args);
+
+            return eval.bounceTailForm(localStore, myBody);
+        }
+    }
 }
