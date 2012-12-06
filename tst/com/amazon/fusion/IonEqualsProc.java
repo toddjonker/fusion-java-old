7ef0852074572a8da9ae119b4b66ef159fa1089c
Todd V. Jonker <todd@toddjonker.net>
2012-12-06 13:46:04 -0800

    Added `ion_equals` for testing.

---
+++ tst/com/amazon/fusion/IonEqualsProc.java
@@ -0,0 +1,28 @@
+// Copyright (c) 2012 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion;
+
+import com.amazon.ion.IonValue;
+
+/** FOR TESTING ONLY */
+final class IonEqualsProc
+    extends Procedure2
+{
+    IonEqualsProc()
+    {
+        //    "                                                                               |
+        super("Compares two ionizable values.",
+              "value1", "value2");
+    }
+
+    @Override
+    Object doApply(Evaluator eval, Object arg0, Object arg1)
+        throws FusionException
+    {
+        IonValue iv0 = eval.convertToIonValueMaybe(arg0);
+        IonValue iv1 = eval.convertToIonValueMaybe(arg1);
+
+        boolean result = iv0 != null && iv0.equals(iv1);
+        return eval.newBool(result);
+    }
+}
