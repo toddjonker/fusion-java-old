01279d991d47ed56df0ea658fab0be86bff9c927
Todd V. Jonker <todd@toddjonker.net>
2013-04-24 10:56:41 -0700

    UnboundIdentifierFailure shouldn't be public

--- src/com/amazon/fusion/UnboundIdentifierFailure.java
+++ src/com/amazon/fusion/UnboundIdentifierFailure.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2012-2013 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion;
 
@@ -6,10 +6,10 @@ package com.amazon.fusion;
  * Indicates a reference to an unbound identifier.
  */
 @SuppressWarnings("serial")
-public final class UnboundIdentifierFailure
+final class UnboundIdentifierFailure
     extends SyntaxFailure
 {
-    public UnboundIdentifierFailure(String name, SyntaxValue... exprs)
+    UnboundIdentifierFailure(String name, SyntaxValue... exprs)
     {
         super(name, "unbound identifier", exprs);
     }
