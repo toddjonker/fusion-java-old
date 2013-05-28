63da571c95dcd853e0137fcf93368ad07995060f
Todd V. Jonker <todd@toddjonker.net>
2013-05-28 14:08:03 -0700

    Added timestamp_to_epoch_millis
    FUSION-106

---
+++ src/com/amazon/fusion/FusionTimestamp.java
@@ -0,0 +1,45 @@
+// Copyright (c) 2012-2013 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion;
+
+import com.amazon.ion.IonTimestamp;
+import com.amazon.ion.IonValue;
+import com.amazon.ion.Timestamp;
+import java.math.BigDecimal;
+
+
+final class FusionTimestamp
+{
+    private FusionTimestamp() {}
+
+
+    static final class TimestampToEpochMillisProc
+        extends Procedure1
+    {
+        TimestampToEpochMillisProc()
+        {
+            //    "                                                                               |
+            super("Given a non-null timestamp, returns the same point in time represented as the\n" +
+                  "number of milliseconds since 1970-01-01T00:00Z.  The result is a decimal.",
+                  "timestamp");
+        }
+
+        @Override
+        Object doApply(Evaluator eval, Object arg)
+            throws FusionException
+        {
+            IonValue iv = FusionValue.castToIonValueMaybe(arg);
+            if (iv instanceof IonTimestamp)
+            {
+                Timestamp ts = ((IonTimestamp) iv).timestampValue();
+                if (ts != null)
+                {
+                    BigDecimal millis = ts.getDecimalMillis();
+                    return eval.newDecimal(millis);
+                }
+            }
+
+            throw new ArgTypeFailure(this, "non-null timestamp", 0, arg);
+        }
+    }
+}
