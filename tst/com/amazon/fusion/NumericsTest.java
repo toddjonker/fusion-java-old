744afdfaa56a46de63825919d4b29105bbf1eb01
Todd V. Jonker <todd@toddjonker.net>
2013-08-12 18:32:13 -0700

    Remove module /fusion/math.
    Its contents have moved to /fusion/number.  FUSION-146

--- tst/com/amazon/fusion/NumericsTest.java
+++ tst/com/amazon/fusion/NumericsTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2012-2013 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion;
 
@@ -14,7 +14,6 @@ public class NumericsTest
     public void requires()
         throws FusionException
     {
-        topLevel().requireModule("/fusion/math");
         topLevel().requireModule("/fusion/timestamp");
     }
 
