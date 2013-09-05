8e2977d16c9f68a027b131f9bef89cd22e4273b4
Todd V. Jonker <todd@toddjonker.net>
2013-09-04 18:45:55 -0700

    Fix bug in `for_list` documentation example.

--- src/com/amazon/fusion/ForListForm.java
+++ src/com/amazon/fusion/ForListForm.java
@@ -22,7 +22,7 @@ final class ForListForm
               "    (for_list\n" +
               "      [ (even [0, 2, 4]),\n" +
               "        (odd  [1, 3, 5]) ]\n" +
-              "      [even, odd])             =>  [1, 5, 9]");
+              "      (+ even odd))             =>  [1, 5, 9]");
     }
 
 
