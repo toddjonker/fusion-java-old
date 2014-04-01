02e82ebff1e7019a37e283233b39059772bd9c26
Todd V. Jonker <todd@toddjonker.net>
2014-04-01 13:17:30 -0700

    Document FusionAssertionException.getUserMessage()

--- src/com/amazon/fusion/FusionAssertionException.java
+++ src/com/amazon/fusion/FusionAssertionException.java
@@ -29,6 +29,11 @@ public final class FusionAssertionException
         addContext(location);
     }
 
+    /**
+     * Returns the formatted message as provided by the application.
+     *
+     * @return may be null if no message values were provided.
+     */
     public String getUserMessage()
     {
         return getBaseMessage();
