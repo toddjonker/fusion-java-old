311c2ef3191444efe2fc8c7eb73cf36ad797b200
Todd V. Jonker <todd@toddjonker.net>
2014-04-30 12:53:27 -0700

    Refactor FUnitTest, extracting SimpleScriptRunner.
    This is more streamlined than using @Injected and
    eliminates a useless parent test for each script.

--- tst/com/amazon/fusion/FUnitTest.java
+++ tst/com/amazon/fusion/FUnitTest.java
@@ -1,101 +1,15 @@
-// Copyright (c) 2012 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2012-2014 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion;
 
-import com.amazon.fusion.junit.Injected;
-import com.amazon.fusion.junit.Injected.Inject;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import org.junit.Test;
+import com.amazon.fusion.junit.SimpleScriptRunner;
 import org.junit.runner.RunWith;
 
 /**
  * Evaluates all the files under the 'ftst' source directory.
  * If evaluation completes without failure, its assumed to be a passing test.
  */
-@RunWith(Injected.class)
+@RunWith(SimpleScriptRunner.class)
 public class FUnitTest
-    extends CoreTestCase
 {
-    @Inject("testFile")
-    public static final File[] FILES =
-        gatherFiles(/* recurse */ true, "ftst");
-
-    private File myTestFile;
-
-    public void setTestFile(File file)
-    {
-        myTestFile = file;
-    }
-
-
-    @Test
-    public void run()
-        throws Exception
-    {
-        useTstRepo();
-        topLevel().load(myTestFile);
-    }
-
-
-    //========================================================================
-
-    // Helper methods forked from com.amazon.ion.TestUtils
-
-    private static void gatherFiles(List<File> results,
-                                    boolean recurse,
-                                    File dir)
-    {
-        String[] fileNames = dir.list();
-        if (fileNames == null)
-        {
-            String message = "Not a directory: " + dir.getAbsolutePath();
-            throw new IllegalArgumentException(message);
-        }
-
-        // Sort the fileNames so they are listed in order.
-        // This is not a functional requirement but it helps humans scanning
-        // the output looking for a specific file.
-        Arrays.sort(fileNames);
-
-        for (String fileName : fileNames)
-        {
-            File testFile = new File(dir, fileName);
-            if (testFile.isDirectory())
-            {
-                if (recurse)
-                {
-                    gatherFiles(results, recurse, testFile);
-                }
-            }
-            else if (fileName.endsWith(".test.fusion"))
-            {
-                results.add(testFile);
-            }
-        }
-    }
-
-    private static File[] gatherFiles(boolean recurse,
-                                      String... directories)
-    {
-        ArrayList<File> files = new ArrayList<File>();
-
-        for (String dirName : directories)
-        {
-            File dir = new File(dirName);
-            if (! dir.isDirectory())
-            {
-                String message =
-                    "not a directory: "
-                        + dir.getAbsolutePath();
-                throw new IllegalArgumentException(message);
-            }
-
-            gatherFiles(files, recurse, dir);
-        }
-
-        return files.toArray(new File[files.size()]);
-    }
 }
---
+++ tst/com/amazon/fusion/junit/SimpleScriptRunner.java
@@ -0,0 +1,133 @@
+// Copyright (c) 2012-2014 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion.junit;
+
+import com.amazon.fusion.FusionRuntime;
+import com.amazon.fusion.FusionRuntimeBuilder;
+import com.amazon.fusion.TopLevel;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.Statement;
+
+/**
+ * Evaluates all the files under the 'ftst' source directory.
+ * If evaluation completes without failure, its assumed to be a passing test.
+ * <p>
+ * Classes using this runner can also use {@link BeforeClass} and
+ * {@link AfterClass} annotations but not {@link Before} or {@link After}.
+ * Similarly, {@link ClassRule}s are supported but not {@link Rule}s.
+ */
+public class SimpleScriptRunner
+    extends ParentRunner<File>
+{
+    /**
+     * Test action that runs a single Fusion script.
+     */
+    private static final class FusionScriptStatement
+        extends Statement
+    {
+        private final SimpleScriptRunner myRunner;
+        private final File myScript;
+
+        private FusionScriptStatement(SimpleScriptRunner runner, File script)
+        {
+            myRunner = runner;
+            myScript = script;
+        }
+
+        @Override
+        public void evaluate()
+            throws Throwable
+        {
+            FusionRuntime r = myRunner.myRuntimeBuilder.build();
+            TopLevel t = r.getDefaultTopLevel();
+            t.load(myScript);
+        }
+    }
+
+
+    //========================================================================
+
+
+    private final FusionRuntimeBuilder myRuntimeBuilder;
+
+
+    public SimpleScriptRunner(Class<?> testClass)
+        throws Throwable
+    {
+        super(testClass);
+
+        FusionRuntimeBuilder b = FusionRuntimeBuilder.standard();
+        b = b.withConfigProperties(testClass, "/fusion.properties");
+
+        File tstRepo = new File("ftst/repo");
+        b.addRepositoryDirectory(tstRepo);
+
+        myRuntimeBuilder = b.immutable();
+    }
+
+
+    @Override
+    protected List<File> getChildren()
+    {
+        ArrayList<File> files = new ArrayList<>();
+        gatherFiles(files, new File("ftst"));
+        return files;
+    }
+
+    @Override
+    protected Description describeChild(File child)
+    {
+        Class<?> klass = getTestClass().getJavaClass();
+        String name = child.getPath();
+        return Description.createTestDescription(klass, name);
+    }
+
+    @Override
+    protected void runChild(File child, RunNotifier notifier)
+    {
+        Statement s = new FusionScriptStatement(this, child);
+        Description d = describeChild(child);
+        runLeaf(s,d, notifier);
+    }
+
+
+    //========================================================================
+
+
+    // Forked from com.amazon.ion.TestUtils
+    private static void gatherFiles(List<File> results,
+                                    File dir)
+    {
+        String[] fileNames = dir.list();
+
+        // Sort the fileNames so they are listed in order.
+        // This is not a functional requirement but it helps humans scanning
+        // the output looking for a specific file.
+        Arrays.sort(fileNames);
+
+        for (String fileName : fileNames)
+        {
+            File testFile = new File(dir, fileName);
+            if (testFile.isDirectory())
+            {
+                gatherFiles(results, testFile);
+            }
+            else if (fileName.endsWith(".test.fusion"))
+            {
+                results.add(testFile);
+            }
+        }
+    }
+}
