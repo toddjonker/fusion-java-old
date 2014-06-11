f7450a0cd23cef9e30edc8a15047dda4dc2ca2f5
Todd V. Jonker <todd@toddjonker.net>
2014-06-11 16:49:32 -0700

    Add FusionProcedure.isProcedure(). FUSION-245

---
+++ src/com/amazon/fusion/FusionProcedure.java
@@ -0,0 +1,37 @@
+// Copyright (c) 2014 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion;
+
+import static com.amazon.fusion.FusionBool.makeBool;
+
+/**
+ * Utilities for Fusion procedures.
+ */
+public final class FusionProcedure
+{
+    private FusionProcedure() {}
+
+
+    /**
+     * Determines whether the given Fusion value is a procedure.
+     *
+     * @param top must not be null.
+     */
+    public static boolean isProcedure(TopLevel top, Object value)
+        throws FusionException
+    {
+        return (value instanceof Procedure);
+    }
+
+
+    static final class IsProcedureProc
+        extends Procedure1
+    {
+        @Override
+        Object doApply(Evaluator eval, Object arg)
+            throws FusionException
+        {
+            return makeBool(eval, arg instanceof Procedure);
+        }
+    }
+}
