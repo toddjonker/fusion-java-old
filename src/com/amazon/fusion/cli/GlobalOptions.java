7a197c8737ceef276924cbbba848189cea68f529
Todd V. Jonker <todd@toddjonker.net>
2014-06-11 14:27:06 -0700

    CLI has global repositories option.

--- src/com/amazon/fusion/cli/GlobalOptions.java
+++ src/com/amazon/fusion/cli/GlobalOptions.java
@@ -4,17 +4,41 @@ package com.amazon.fusion.cli;
 
 import com.amazon.fusion.FusionRuntimeBuilder;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  *
  */
 class GlobalOptions
 {
-    String myBootstrapPath;
+    private String          myBootstrapPath;
+    private ArrayList<File> myRepositories;
+
 
     public void setBootstrapRepository(String path)
     {
         myBootstrapPath = path;
+    }
+
+
+    public void setRepositories(String pathList)
+    {
+        if (myRepositories == null) myRepositories = new ArrayList<>();
+
+        StringTokenizer tokenizer =
+            new StringTokenizer(pathList, File.pathSeparator);
+
+        while (tokenizer.hasMoreTokens())
+        {
+            String token = tokenizer.nextToken().trim();
+
+            if (token.length() != 0)
+            {
+                File file = new File(token);
+                myRepositories.add(file);
+            }
+        }
     }
 
 
@@ -34,6 +58,22 @@ class GlobalOptions
             catch (IllegalArgumentException e)
             {
                 String message = "bootstrapRepository: " + e.getMessage();
+                throw new UsageException(message);
+            }
+        }
+
+        if (myRepositories != null)
+        {
+            try
+            {
+                for (File repo : myRepositories)
+                {
+                    builder.addRepositoryDirectory(repo);
+                }
+            }
+            catch (IllegalArgumentException e)
+            {
+                String message = "repositories: " + e.getMessage();
                 throw new UsageException(message);
             }
         }
