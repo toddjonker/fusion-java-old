685100594074213d59ce76410195d817ef4df184
Todd V. Jonker <todd@toddjonker.net>
2014-06-25 16:17:26 -0700

    Rewrite InterruptionTest for robustness and coverage.
    Now we're not subject to timing constraints.
    We also cover all the major entry points.
    
    Code review ID 2562691

--- tst/com/amazon/fusion/InterruptionTest.java
+++ tst/com/amazon/fusion/InterruptionTest.java
@@ -2,9 +2,13 @@
 
 package com.amazon.fusion;
 
+import static com.amazon.fusion.FusionVoid.voidValue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import com.amazon.ion.IonReader;
+import java.io.File;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 /**
@@ -37,39 +41,208 @@ public class InterruptionTest
     }
 
 
+    private static final String INTERRUPT_MODULE = "/interrupt";
+
+    private static class RequestInterruptProc
+        extends Procedure0
+    {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        @Override
+        Object doApply(Evaluator eval)
+        {
+            latch.countDown();
+            return voidValue(eval);
+        }
+    }
+
+
+    private void expectInterruption(EvaluationTask task)
+        throws Exception
+    {
+        useTstRepo();
+
+        RequestInterruptProc latchProc = new RequestInterruptProc();
+        ModuleBuilder mb = runtime().makeModuleBuilder(INTERRUPT_MODULE);
+        mb.define("ready_for_interrupt", latchProc);
+        mb.instantiate();
+
+        Thread thread = new Thread(task);
+        thread.start();
+
+        if (! latchProc.latch.await(5, TimeUnit.SECONDS))
+        {
+            if (task.myEvaluationFinished)
+            {
+                throw new AssertionError("Task finished before latch");
+            }
+
+            if (task.myCaughtThrowable != null)
+            {
+                throw new AssertionError("Task threw exception before latch",
+                                         task.myCaughtThrowable);
+            }
+            fail("latch timed out");
+        }
+
+        thread.interrupt();
+        thread.join();
+
+        assertFalse(task.myEvaluationFinished);
+
+        if (! (task.myCaughtThrowable instanceof FusionInterruptedException))
+        {
+            throw new AssertionError("Unexpected exception",
+                                     task.myCaughtThrowable);
+        }
+    }
+
+
+    //========================================================================
+
+
     @Test
-    public void threadInterruptStopsEvaluation()
+    public void interruptMakeTopLevel()
         throws Exception
     {
         // Bootstrap our FusionRuntime with the bare minimum.
         runtimeBuilder().setDefaultLanguage("/fusion/private/kernel");
-        final FusionRuntime runtime = runtime();
 
         EvaluationTask task = new EvaluationTask()
         {
             @Override
             void eval() throws Exception
             {
-                runtime.makeTopLevel("/fusion");
+                runtime().makeTopLevel("/interrupt_on_instantiation");
             }
         };
 
-        final Thread thread = new Thread(task);
-        thread.start();
-
-        Thread.sleep(10);
-        assertNull(task.myCaughtThrowable);
-        assertFalse("Slept for too long, all of /fusion was loaded",
-                    task.myEvaluationFinished);
-
-        thread.interrupt();
-        thread.join();
-        assertTrue(task.myCaughtThrowable instanceof FusionInterruptedException);
-        assertFalse(task.myEvaluationFinished);
+        expectInterruption(task);
 
         // The code above aborts compiling and loading of /fusion.
         // Now check that we can can successfully load the rest of /fusion.
 
-        runtime.makeTopLevel("/fusion");
+        runtime().makeTopLevel("/fusion");
+    }
+
+
+    @Test
+    public void interruptRequireModule()
+        throws Exception
+    {
+        EvaluationTask task = new EvaluationTask()
+        {
+            @Override
+            void eval() throws Exception
+            {
+                TopLevel top = topLevel();
+                top.requireModule("/interrupt_on_instantiation");
+            }
+        };
+
+        expectInterruption(task);
+    }
+
+
+    private static final String INTERRUPTABLE_EXPRESSION =
+        "(begin (ready_for_interrupt) (let loop [] (loop)))";
+
+    @Test
+    public void interruptEvalReader()
+        throws Exception
+    {
+        EvaluationTask task = new EvaluationTask()
+        {
+            @Override
+            void eval() throws Exception
+            {
+                TopLevel top = topLevel();
+                top.requireModule(INTERRUPT_MODULE);
+
+                IonReader reader =
+                    system().newReader(INTERRUPTABLE_EXPRESSION);
+                top.eval(reader);
+            }
+        };
+
+        expectInterruption(task);
+    }
+
+
+    @Test
+    public void interruptEvalString()
+        throws Exception
+    {
+        EvaluationTask task = new EvaluationTask()
+        {
+            @Override
+            void eval() throws Exception
+            {
+                TopLevel top = topLevel();
+                top.requireModule(INTERRUPT_MODULE);
+                top.eval(INTERRUPTABLE_EXPRESSION);
+            }
+        };
+
+        expectInterruption(task);
+    }
+
+
+    @Test
+    public void interruptLoad()
+        throws Exception
+    {
+        EvaluationTask task = new EvaluationTask()
+        {
+            @Override
+            void eval() throws Exception
+            {
+                TopLevel top = topLevel();
+                top.load(new File("tst-data/interrupt.fusion"));
+            }
+        };
+
+        expectInterruption(task);
+    }
+
+
+    @Test
+    public void interruptCallByName()
+        throws Exception
+    {
+        EvaluationTask task = new EvaluationTask()
+        {
+            @Override
+            void eval() throws Exception
+            {
+                TopLevel top = topLevel();
+                top.requireModule(INTERRUPT_MODULE);
+                top.eval("(define (proc) " + INTERRUPTABLE_EXPRESSION + ")");
+                top.call("proc");
+            }
+        };
+
+        expectInterruption(task);
+    }
+
+
+    @Test
+    public void interruptCallByValue()
+        throws Exception
+    {
+        EvaluationTask task = new EvaluationTask()
+        {
+            @Override
+            void eval() throws Exception
+            {
+                TopLevel top = topLevel();
+                top.requireModule(INTERRUPT_MODULE);
+                top.eval("(define (proc) " + INTERRUPTABLE_EXPRESSION + ")");
+                Object proc = top.lookup("proc");
+                top.call(proc);
+            }
+        };
+
+        expectInterruption(task);
     }
 }
