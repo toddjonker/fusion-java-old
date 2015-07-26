3ce6b395284d9ac649b094fbb0b596a61bf91f23
Todd V. Jonker <todd@toddjonker.net>
2015-07-25 18:24:03 -0700

    Test FusionValue.isAnnotated()

--- tst/com/amazon/fusion/FusionValueTest.java
+++ tst/com/amazon/fusion/FusionValueTest.java
@@ -1,9 +1,16 @@
-// Copyright (c) 2012 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2012-2015 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion;
 
+import static com.amazon.fusion.FusionSexp.isEmptySexp;
+import static com.amazon.fusion.FusionSexp.isSexp;
+import static com.amazon.fusion.FusionSexp.unsafePairHead;
+import static com.amazon.fusion.FusionSexp.unsafePairTail;
+import static com.amazon.fusion.FusionValue.isAnnotated;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import com.amazon.ion.IonInt;
 import com.amazon.ion.IonValue;
 import org.junit.Test;
@@ -34,5 +41,29 @@ public class FusionValueTest
     {
         Object fv = eval("(lambda () 12)");
         FusionValue.copyToIonValue(fv, system());
+    }
+
+    @Test
+    public void testIsAnnotated()
+        throws Exception
+    {
+        useTstRepo();
+        TopLevel top = topLevel();
+        Evaluator eval = evaluator();
+        top.requireModule("/testutils");
+
+        Object values = top.lookup("representative_ion_data");
+        assertTrue(isSexp(eval, values));
+
+        while (! isEmptySexp(eval, values))
+        {
+            Object value = unsafePairHead(eval, values);
+            assertFalse(isAnnotated(eval, value));
+
+            value = top.call("annotate", value, "ann");
+            assertTrue(value.toString(), isAnnotated(eval, value));
+
+            values = unsafePairTail(eval, values);
+        }
     }
 }
