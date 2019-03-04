4cd1cda6e006e98dcb8b9231dd4af52702b6dc13
Todd V. Jonker <todd@toddjonker.net>
2019-03-03 16:55:03 -0800

    Improve test coverage of SourceLocation.forCurrentSpan().
    This exposes an undesirable IllegalStateException when the IonReader cannot provide a span.  We should treat that as an "unknown location" case.

--- tst/com/amazon/fusion/SourceLocationTest.java
+++ tst/com/amazon/fusion/SourceLocationTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2017 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2014-2019 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion;
 
@@ -13,40 +13,63 @@ import org.junit.Test;
 
 public class SourceLocationTest
 {
+    private void assertNoLocation(IonReader ir)
+    {
+        SourceLocation loc = SourceLocation.forCurrentSpan(ir, null);
+        assertNull("expected null SourceLocation", loc);
+
+        SourceName name = SourceName.forDisplay("test source");
+        loc = SourceLocation.forCurrentSpan(ir, name);
+        assertEquals("unknown location in test source", loc.display());
+    }
+
+    private void assertLocation(String expectedOffsets, IonReader ir)
+    {
+        SourceLocation loc = SourceLocation.forCurrentSpan(ir, null);
+        assertEquals(expectedOffsets, loc.display());
+
+        SourceName name = SourceName.forDisplay("test source");
+        loc = SourceLocation.forCurrentSpan(ir, name);
+        assertEquals(expectedOffsets + " of test source", loc.display());
+    }
+
+
     @Test
     public void testLocationDisplay()
     {
         IonSystem sys = IonSystemBuilder.standard().build();
         IonDatagram dg = sys.getLoader().load("(hi)");
+
+        // Binary reader doesn't display offsets.
+        // TODO Display offsets in binary data.
+
         IonReader ir = sys.newReader(dg.getBytes());
 
-        // Binary position, no name.
+        assertNoLocation(ir);  // Before first value
+
         ir.next();
-        SourceLocation loc = SourceLocation.forCurrentSpan(ir, null);
-        loc = SourceLocation.forCurrentSpan(ir, null);
-        assertNull(loc);
+        assertNoLocation(ir);
 
-        // Binary position, with name
-        SourceName name = SourceName.forDisplay("test source");
-        loc = SourceLocation.forCurrentSpan(ir, name);
-        assertEquals("unknown location in test source",
-                     loc.display());
-
-        // Text position, no name
-        ir = sys.newReader("(hi)");
-        ir.next();
-        loc = SourceLocation.forCurrentSpan(ir, null);
-        assertEquals("1st line, 1st column",
-                     loc.display());
-
-        // Text position, with name
-        loc = SourceLocation.forCurrentSpan(ir, name);
-        assertEquals("1st line, 1st column of test source",
-                     loc.display());
         ir.stepIn();
+        assertNoLocation(ir);  // Before first child
+
+
+        // Text reader gives line/column locations
+
+        ir = sys.newReader("(hi)");
+
+        assertNoLocation(ir);  // Before first value
+
         ir.next();
-        loc = SourceLocation.forCurrentSpan(ir, name);
-        assertEquals("1st line, 2nd column of test source",
-                     loc.display());
+        assertLocation("1st line, 1st column", ir);
+
+        ir.stepIn();
+        assertNoLocation(ir);  // Before first child
+
+        ir.next();
+        assertLocation("1st line, 2nd column", ir);
+
+        ir.next();
+        assertNoLocation(ir);  // After last child
     }
 }
