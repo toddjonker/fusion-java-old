6388d873c7dc63692ddf99a2fc7c4d38c0e0effb
Todd V. Jonker <todd@toddjonker.net>
2019-04-01 17:46:48 -0700

    Port Racket's implementation of `for_fold`.
    This provides a more extensible foundation for the comprehension forms.
    
    I wanted to start adding more of Racket's [`for`-family of iteration forms](https://docs.racket-lang.org/reference/for.html) but quickly realized that the approach I'd taken for `for_list` and `fors_list` was not going to scale. Doing some research, I realized that it would be better to take Racket's approach by building up from `for/fold/derived` so all the iteration and collation logic can be reused. See the implementation notes in `for.fusion` for more details.
    
    Code review ID CR-10491491

---
+++ rkt/forfold_eval_order.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require rackunit)
+
+;; This corresponds to the evaluation-order test case in ftst/for.test.fusion
+
+(let* [(evaluation_order '())
+       (record (lambda (v) (set! evaluation_order (cons v evaluation_order)) v))]
+  (check-equal?
+    (reverse
+      (for/fold
+       [(v (begin (record "v") '()))]
+       [(a (begin (record "a") (sequence-map record (quote (1 3)))))
+        (b (begin (record "b") (sequence-map record (quote (2 4)))))]
+      (record "body")
+      (cons (list a b) v)))
+    '((1 2) (3 4)))
+  ; It's unexpected that the accumulator v is initialized after the
+  ; sequences a and b, but that's how it works in Racket 6.5.
+  (check-equal?
+    (reverse evaluation_order)
+    '("a" "b" "v" 1 2 "body" 3 4 "body")))
