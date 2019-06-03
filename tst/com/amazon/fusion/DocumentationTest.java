toddjonker/fusion-java-old

commit c04e9199f506db6995bed47f097ad35487b29d4d

Enabling `fusion eval` as well as `fusion require` with command chainability now more coherent (i.e. common top-level as intended)
Samples:
`fusion eval "(+ 2 3)"`
==>
`5`

`fusion require "/fusioncontrib/fpath" \; eval "(fpath_select {x: [{y: 4},{y: 5},{z: 6}]} x * y % (|n| (<= n 5)))"`
==>
`[4, 5]`

`fusion help` snapshotted with these additions: 

Note that this is being kept as the same CR (Code review ID CR-7003367 ) to maintain history of comments and intents.
Though it is notable that from revision #3 it's doing a few things fundamentally different:
* Instead of splitting Load into file-based and inline-based, keeping 2 separate commands -- Load and Eval -- as suggested
** Didn't attempt to distribute out the commonality between Load and Eval but may do so
* It's maintaining shared state on the top-level across commands (or maintaining a runtime more correctly, and top-level is guaranteed unique per runtime)
** The intent with the ";" sub-command splitting was to share a runtime (if there's a different / preferred global-across-commands approach, we can evolve or converge to)
* Added Require to the important trifecta of primitives
* Had chew_stdin been in standard library (or even _non_-standard but loadable under proper dir), this could've given me all I was looking for...
** ...But we'll save the STDIN slurping improving for another change (`for_list` / `for` etc. super-powered to understand streams not just lists one day soon-ish)

Code review ID CR-7003367

src/com/amazon/fusion/cli/CommandFactory.java
@@ -18,6 +18,8 @@ class CommandFactory
             {
                 new Repl(),
                 new Load(),
+                new Eval(),
+                new Require(),
                 new Cover(),
                 new Separator(),
                 new Help(),

src/com/amazon/fusion/cli/Eval.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2014 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2012-2019 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion.cli;
 
@@ -7,25 +7,25 @@ import static com.amazon.fusion.FusionVoid.isVoid;
 import com.amazon.fusion.ExitException;
 import com.amazon.fusion.FusionException;
 import com.amazon.fusion.TopLevel;
-import java.io.File;
 
 
-class Load
+class Eval
     extends Command
 {
     //=+===============================================================================
     private static final String HELP_ONE_LINER =
-        "Load and evaluate a script.";
+        "Evaluate an inline script.";
     private static final String HELP_USAGE =
-        "load FILE";
+        "eval EVAL_TEXT";
     private static final String HELP_BODY =
-        "Loads and evaluates the Fusion script in the given FILE.  If the result of the\n" +
-        "last expression is not void, it is sent to standard output via `write`.";
+        "Evaluate an inline script from the given EVAL_TEXT. If the result of the\n" +
+        "last expression is not void, it is sent to standard output via `write`.\n" + 
+        "Be aware that regular shell quoting/escaping rules take place first.";
 
 
-    Load()
+    Eval()
     {
-        super("load");
+        super("eval");
         putHelpText(HELP_ONE_LINER, HELP_USAGE, HELP_BODY);
     }
 
@@ -38,30 +38,24 @@ class Load
     {
         if (args.length != 1) throw usage();
 
-        String fileName = args[0];
-        if (fileName.length() == 0) throw usage();
+        String evalText = args[0];
+        if (evalText.length() == 0) throw usage();
 
-        File file = new File(fileName);
-        if (! (file.canRead() && file.isFile()))
-        {
-            throw usage("Not a readable file: " + fileName);
-        }
-
-        return new Executor(globals, file);
+        return new Executor(globals, evalText);
     }
 
 
     static class Executor
         extends FusionExecutor
     {
-        final File myFile;
+        final String myEvalText;
 
 
-        Executor(GlobalOptions globals, File file)
+        Executor(GlobalOptions globals, String evalText)
         {
             super(globals);
 
-            myFile = file;
+            myEvalText = evalText;
         }
 
 
@@ -73,7 +67,7 @@ class Load
             {
                 TopLevel top = runtime().getDefaultTopLevel();
 
-                Object result = top.load(myFile);
+                Object result = top.eval(myEvalText);
 
                 if (result instanceof Object[])
                 {

src/com/amazon/fusion/cli/FusionExecutor.java
@@ -32,12 +32,7 @@ abstract class FusionExecutor
 
     FusionRuntime runtime()
         throws FusionException, UsageException
-    {
-        if (myRuntime == null)
-        {
-            FusionRuntimeBuilder builder = runtimeBuilder();
-            myRuntime = builder.build();
-        }
-        return myRuntime;
+    { 
+        return myGlobalOptions.runtime();
     }
 }

src/com/amazon/fusion/cli/GlobalOptions.java
@@ -3,6 +3,8 @@
 package com.amazon.fusion.cli;
 
 import static com.amazon.fusion.FusionRuntimeBuilder.PROPERTY_BOOTSTRAP_REPOSITORY;
+import com.amazon.fusion.FusionException;
+import com.amazon.fusion.FusionRuntime;
 import com.amazon.fusion.FusionRuntimeBuilder;
 import com.amazon.ion.IonException;
 import java.io.File;
@@ -42,6 +44,7 @@ class GlobalOptions
     private String          myBootstrapPath;
     private ArrayList<File> myRepositories;
     private ArrayList<File> myCatalogs;
+    private FusionRuntime   myRuntime;
 
 
     public void setBootstrapRepository(String path)
@@ -137,5 +140,17 @@ class GlobalOptions
         }
 
         return builder;
+    }
+
+    FusionRuntime runtime()
+        throws FusionException, UsageException
+    {
+        // Lazily loading a runtime that can be global across commands, centralized more vs original FusionExecutor implementation
+        if (myRuntime == null)
+        {
+            FusionRuntimeBuilder builder = runtimeBuilder();
+            myRuntime = builder.build();
+        }
+        return myRuntime;
     }
 }

src/com/amazon/fusion/cli/Require.java
@@ -1,0 +1,80 @@
+// Copyright (c) 2012-2019 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion.cli;
+
+import com.amazon.fusion.FusionException;
+import com.amazon.fusion.TopLevel;
+
+
+class Require
+    extends Command
+{
+    //=+===============================================================================
+    private static final String HELP_ONE_LINER =
+        "Requires a single Fusion module.";
+    private static final String HELP_USAGE =
+        "require MODULE_NAME";
+    private static final String HELP_BODY =
+        "Requires a single Fusion module. Chain commands to require multiple modules\n" +
+        "By itself, Fusion produces effect to output, but opens up modules for subsequent commands\n" + 
+        "Ex: ` fusion require /fusion/struct ';' eval '(begin (define s (mutable_struct \"a\" 1)) (put_m s \"modified\" true) s)' ` ==> {a:1,modified:true}\n" +
+        "Be aware that regular shell quoting/escaping rules take place first.";
+
+
+    Require()
+    {
+        super("require");
+        putHelpText(HELP_ONE_LINER, HELP_USAGE, HELP_BODY);
+    }
+
+
+    @Override
+    Executor makeExecutor(GlobalOptions globals,
+                          Object        options,
+                          String[]      args)
+        throws UsageException
+    {
+        if (args.length != 1) throw usage();
+
+        String moduleName = args[0];
+        if (moduleName.length() == 0) throw usage();
+
+        return new Executor(globals, moduleName);
+    }
+
+
+    static class Executor
+        extends FusionExecutor
+    {
+        final String myModulePath;
+
+
+        Executor(GlobalOptions globals, String modulePath)
+        {
+            super(globals);
+
+            myModulePath = modulePath;
+        }
+
+
+        @Override
+        public int execute()
+            throws Exception
+        {
+            try
+            {
+                TopLevel top = runtime().getDefaultTopLevel();
+
+                top.requireModule(myModulePath);
+            }
+            catch (FusionException e)
+            {
+                // TODO optionally display the stack trace
+                System.err.println(e.getMessage());
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}

commit bbc7f20630870bb02deef10d0e74e262f74d322b

Minimal test coverage for generated Fusion docs.
This is just enough to trap the bug introduced by c04e9199 in which the CLI's runtime during `fusion document` wasn't configured to collect documentation, leaving almost nothing documented.

tst/com/amazon/fusion/DocumentationTest.java
@@ -1,0 +1,94 @@
+// Copyright (c) 2019 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.fusion;
+
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HTMLParserListener;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.io.IOException;
+import java.net.URL;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DocumentationTest
+{
+    private WebClient myWebClient;
+
+    @Before
+    public void initWebClient()
+    {
+        myWebClient = new WebClient();
+
+        myWebClient.setIncorrectnessListener(new IncorrectnessListener()
+        {
+            @Override
+            public void notify(String s, Object o)
+            {
+                fail("Incorrectness: " + s);
+            }
+        });
+
+        myWebClient.setHTMLParserListener(new HTMLParserListener()
+        {
+            @Override
+            public void error(String message, URL url, String html,
+                              int line, int column, String key)
+            {
+                fail("HTML error at line " + line + ": " + message);
+            }
+
+            @Override
+            public void warning(String message, URL url, String html,
+                                int line, int column, String key)
+            {
+                // TODO Enable this and fix warnings.
+//              fail("HTML warning at line " + line + ": " + message);
+            }
+        });
+    }
+
+    @After
+    public void tearDown()
+    {
+        myWebClient = null;
+    }
+
+    private HtmlPage loadModule(String modulePath)
+        throws IOException
+    {
+        String   url  = "file:build/brazil-documentation" + modulePath + ".html";
+        HtmlPage page = myWebClient.getPage(url);
+        assertEquals(modulePath, page.getTitleText());
+
+        HtmlElement  body    = page.getBody();
+        HtmlHeading1 firstH1 = body.getFirstByXPath("h1");
+        assertEquals("Module " + modulePath, firstH1.getTextContent());
+
+        return page;
+    }
+
+
+    /**
+     * Assumes that we've generated our documentation tree!
+     */
+    @Test
+    public void testFusionDoc()
+        throws Exception
+    {
+        HtmlPage    page = loadModule("/fusion");
+        HtmlElement body = page.getBody();
+
+        HtmlElement p = body.getFirstByXPath("p");
+        assertNotNull("missing first <p>", p);
+        assertThat(p.getTextContent(), startsWith("The main Fusion language."));
+    }
+}