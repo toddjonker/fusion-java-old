9d0d2014de77be712bf778281cc2d1d09f301890
Todd V. Jonker <todd@toddjonker.net>
2024-10-12 15:45:49 -0700

    Decouple DocumentTest from project layout.
    Code review ID CR-154029449

--- tst/com/amazon/fusion/cli/DocumentTest.java
+++ tst/com/amazon/fusion/cli/DocumentTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2019-2024 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.fusion.cli;
 
@@ -6,35 +6,36 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import java.io.File;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class DocumentTest
     extends CliTestCase
 {
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
+
     private File noSuchFile()
+        throws Exception
     {
-        File f = new File("no-such-file");
+        File f = new File(tmpDir.getRoot(), "no-such-file");
         assertFalse(f.exists());
         return f;
     }
 
     private File plainFile()
+        throws Exception
     {
-        File f = new File("Config");
+        File f = tmpDir.newFile();
         assert(f.isFile());
         return f;
     }
 
     private File outputDir()
+        throws Exception
     {
-        return new File("build/private/docs-test-output");
-    }
-
-    private File nonRepoDir()
-    {
-        File d = new File("build/private");
-        assert(d.isDirectory());
-        return d;
+        return tmpDir.newFolder("docs-test-output");
     }
 
 
@@ -77,7 +78,7 @@ public class DocumentTest
     public void testRepoHasNoSrc()
         throws Exception
     {
-        String repo = nonRepoDir().getPath();
+        String repo = tmpDir.newFolder().getPath();
 
         run(1, "document", outputDir().getPath(), repo);
 
